992f6114b517b2ca4fc15731e00ae7875cf88106
Ciprian Ionut Iacob <48355427+ciprianiacob@users.noreply.github.com>
2024-01-16 10:19:08 +0000

    Create  fetch-init-04.js

---
+++ fetch-init-04.js
@@ -0,0 +1,21 @@
+// External link to the JSON file
+const jsonUrl = 'https://dohomeart.co.uk/v2/provider/';
+
+// Fetch data from the external JSON file
+fetch(jsonUrl)
+	.then(response => response.json())
+	.then(data => {
+		$(document).ready(function() {
+			$('#sb_booking_content .category-item').each(function(providerIndex) {
+				var target = $(this).find('.item__content > .title--h4');
+        if(target) {
+        	target.text(data[providerIndex].name); 
+        }
+			});
+		});
+
+		console.log(data);
+	})
+	.catch(error => {
+		console.error('Error fetching the JSON data:', error);
+	});
