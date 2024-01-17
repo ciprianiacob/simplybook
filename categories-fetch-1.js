475e573c029a045d7525773d0e755ac378620062
Ciprian Ionut Iacob <48355427+ciprianiacob@users.noreply.github.com>
2024-01-17 07:02:34 +0000

    Create categories-fetch-1.js

---
+++ categories-fetch-1.js
@@ -0,0 +1,13 @@
+const jsonServiceUrl = 'https://dohomeart.co.uk/v2/ext/category/';
+
+fetch(jsonCategoryUrl)
+  .then(response => response.json())
+  .then(data => {
+    $(document).ready(function() {
+    });
+
+    console.log("Category:", data);
+  })
+  .catch(error => {
+    console.error('Error fetching the JSON data:', error);
+  });
