d4ecec3ce19cd3c23773305b7930822463d0defc
Ciprian Ionut Iacob <48355427+ciprianiacob@users.noreply.github.com>
2024-02-06 10:29:36 +0000

    Create  preloader-7.js

---
+++ preloader-7.js
@@ -0,0 +1,45 @@
+async function setPreloaderStatus() {
+	delay(1).then(function() {
+		var preloader = document.getElementById('preloader');
+		var isRunning = preloader && isElementAnimating(preloader);
+		var targetElement = document.querySelector("html");
+
+		if (isRunning) {
+			targetElement.classList.add("preloader-is-not-active");
+		} else {
+			targetElement.classList.remove("preloader-is-not-active");
+		}
+	});
+}
+
+function delay(ms) {
+	return new Promise(function(resolve) {
+		setTimeout(resolve, ms);
+	});
+}
+
+function isElementAnimating(element) {
+	var isAnimating = false;
+	var computedStyle = window.getComputedStyle(element);
+
+	if (computedStyle.getPropertyValue('display') === 'none') {
+		isAnimating = true;
+	}
+
+	return isAnimating;
+}
+
+var preloaderElement = document.getElementById('preloader');
+if (preloaderElement) {
+	var observer = new MutationObserver(function(mutationsList, observer) {
+		setPreloaderStatus();
+	});
+
+	observer.observe(preloaderElement, {
+		attributes: true,
+		attributeFilter: ['style'],
+		childList: true
+	});
+}
+
+setPreloaderStatus();
