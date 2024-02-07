0cfa89382c8ee99b021d94df337dc82336ae07df
Ciprian Ionut Iacob <48355427+ciprianiacob@users.noreply.github.com>
2024-02-07 09:25:50 +0000

    Create  preloader-33.js

---
+++ preloader-33.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function() {
+    
+
+// Get network resources timing information
+var resources = window.performance.getEntriesByType('resource');
+
+// Calculate network statistics
+resources.forEach(function(resource) {
+    console.log('Resource URL: ' + resource.name);
+    console.log('Resource load time: ' + resource.duration + ' milliseconds');
+    console.log('Resource size: ' + resource.transferSize + ' bytes');
+    // Add more calculations as needed
+});
+
+// Get timing information
+var timing = window.performance.timing;
+
+// Calculate timing statistics
+var navigationStart = timing.navigationStart;
+var pageLoadTime = timing.loadEventEnd - navigationStart;
+
+console.log('DOM content loaded: ' + domContentLoadedTime + ' milliseconds');
+console.log('Page load time: ' + pageLoadTime + ' milliseconds');
+  
+});
