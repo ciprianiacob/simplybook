e4d9252b6db669413efdd6d91440b10ff8c7a859
Ciprian Ionut Iacob <48355427+ciprianiacob@users.noreply.github.com>
2024-02-08 05:22:12 +0000

    Create  preloader-68.js

---
+++ preloader-68.js
@@ -0,0 +1,81 @@
+var domDoHomeArtDomContentLoadedTime = 0;
+var domDoHomeArtDomLoadTime = 0;
+var delayDoHomeArtValue = (domDoHomeArtDomContentLoadedTime + domDoHomeArtDomLoadTime) * 1000;
+var finalDelayDoHomeArtValue = 0;
+var preloaderDoHomeArtElement  = "";
+
+function calculateDoHomeArtTimingStatistics() {
+	try {
+		var timing = performance.timing;
+
+		if (!timing || timing.domContentLoadedEventEnd <= 0 || timing.loadEventEnd <= 0) {
+			console.warn("Timing data is incomplete or invalid.");
+			return;
+		}
+
+		var domContentLoadedTime = timing.domContentLoadedEventEnd - timing.navigationStart;
+		var loadTime = timing.loadEventEnd - timing.navigationStart;
+
+		var domContentLoadedTimeSeconds = (domContentLoadedTime + 5) / 1000;
+		var loadTimeSeconds = (loadTime + 5) / 1000;
+		finalDelayDoHomeArtValue = `${(domContentLoadedTimeSeconds + loadTimeSeconds).toFixed(2)}`;
+
+    preloaderDoHomeArtElement  = document.getElementById('preloader');
+
+		console.log(`DOMContentLoaded: ${domContentLoadedTimeSeconds.toFixed(2)} s`);
+		console.log(`Load: ${loadTimeSeconds.toFixed(2)} s`);
+		console.log(`DELAY TIME IN SECONDS: ${(domContentLoadedTimeSeconds + loadTimeSeconds).toFixed(2)} s`);
+		console.log("FINAL DELAY TIME IN SECONDS: ", finalDelayDoHomeArtValue);
+	} catch (error) {
+		console.error("Error calculating timing statistics:", error);
+	}
+}
+
+async function setDoHomeArtPreloaderStatus() {
+	var targetElement = document.querySelector("html");
+	if (targetElement.classList.contains("preloader-is-not-active")) {
+		return;
+	}
+
+	await calculateDoHomeArtTimingStatistics();
+
+	await delayDoHomeArt(finalDelayDoHomeArtValue);
+	var isRunning = preloaderDoHomeArtElement  && isDoHomeArtElementAnimating(preloaderDoHomeArtElement );
+
+	if (isRunning) {
+		targetElement.classList.add("preloader-is-not-active");
+	} else {
+		targetElement.classList.remove("preloader-is-not-active");
+	}
+}
+
+function delayDoHomeArt(ms) {
+	return new Promise(function(resolve) {
+		setTimeout(resolve, ms);
+	});
+}
+
+function isDoHomeArtElementAnimating(element) {
+	var isAnimating = false;
+	var computedStyle = window.getComputedStyle(element);
+
+	if (computedStyle.getPropertyValue('display') === 'none') {
+		isAnimating = true;
+	}
+
+	return isAnimating;
+}
+
+if (preloaderDoHomeArtElement) {
+	var observerDoHomeArt = new MutationObserver(function(mutationsList, observerDoHomeArt) {
+		setDoHomeArtPreloaderStatus();
+	});
+
+	observerDoHomeArt.observe(preloaderDoHomeArtElement, {
+		attributes: true,
+		attributeFilter: ['style'],
+		childList: true
+	});
+}
+
+setDoHomeArtPreloaderStatus();
