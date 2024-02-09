803d5d9bde43158445e04a383b2852280302ccce
Ciprian Ionut Iacob <48355427+ciprianiacob@users.noreply.github.com>
2024-02-09 08:46:00 +0000

    Create  initBookingSteps-39.js

---
+++ initBookingSteps-39.js
@@ -0,0 +1,50 @@
+async function initBookingSteps() {
+    await checkPreloaderStatus();
+    const targetElement = document.querySelector("html");
+    const root = document.querySelector(":root");
+
+    if (typeof finalDelayDoHomeArtValue === 'number' && !isNaN(finalDelayDoHomeArtValue)) {
+        setTimeout(async function() {
+            const stepContent = document.querySelector('.step-content');
+
+            if (stepContent) {
+                targetElement.classList.remove("preloader-is-not-active");
+                domDoHomeArtDomContentLoadedTime = 0;
+                domDoHomeArtDomLoadTime = 0;
+                delayDoHomeArtValue = (domDoHomeArtDomContentLoadedTime + domDoHomeArtDomLoadTime) * 1000;
+                finalDelayDoHomeArtValue = 0;
+                preloaderDoHomeArtElement = null;
+                root.style.setProperty("--main-content-bg", 'rgba(255,255,255,1)');
+                await checkPreloaderStatus();
+
+                switch (true) {
+                    case stepContent.classList.contains('location-step') && !document.getElementById('sb_prerequisites_step_container'):
+                        setTimeout(filterLocations, 1000);
+                        break;
+                    case stepContent.classList.contains('category-step'):
+                        setTimeout(initProviders, 1000);
+                        break;
+                    case stepContent.classList.contains('service-step'):
+                        setTimeout(filterServices, 1000);
+                        break;
+                    case stepContent.classList.contains('paid-attribute-step'):
+                        break;
+                    case stepContent.classList.contains('datetime-step'):
+                        setTimeout(initAvailability, 6000);
+                        break;
+                    default:
+                }
+            }
+        }, 100);
+    } else {
+        console.error("Error: finalDelayDoHomeArtValue is not a valid number.");
+    }
+}
+
+async function waitUntilCheckPreloaderStatusCompletion() {
+    await waitForCheckPreloaderStatusCompletion();
+    const delayValue = await calculateDoHomeArtTimingStatistics();
+    setTimeout(initBookingSteps, 500);
+}
+
+waitUntilCheckPreloaderStatusCompletion();
